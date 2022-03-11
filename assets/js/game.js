d31d35ef7633e8f85e6e05c2efcdc9d0017950ad
Michael Houston <mwtx94@yahoo.com>
2022-03-10 18:25:50 -0600

    added somebeginner functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// added console log to function
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+// playerName is ready for battle!
+console.log(playerName  + " is ready for battle!");
+// this will do math and log 20
+console.log(10+10);
+// our robot's name is
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+// fight();
